5965e6223b4eba7b2957a79c5e4622581dea32e5
varun10p <varun10p@gmail.com>
2020-02-22 14:09:53 -0800

    fuk https

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <h1>Register</h1>
+  <input type="email"
+    name="email"
+    v-model="email"
+    placeholder="email"/>
+  <br>
+  <input type="password"
+    name="password"
+    v-model="password"
+    placeholder="password"/>
+    <br>
+    <button
+       @click="register">
+       Register
+    </button>
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      email: '',
+      passowrd: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        passowrd: this.passowrd
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
